163b19ecad9b5392d50ec4b380b7bb10b5dcedc5
udiste66 <Fabio@Fabios-MacBook-Pro.local>
2016-04-28 07:11:40 +0200

    Docstrings
    Changed comments to doctorings inside the functions.

--- fill-in-the-blanks.py
+++ fill-in-the-blanks.py
@@ -6,15 +6,15 @@ answers1 = ['omelette', 'eggs', 'fried', 'pan']
 answers2 = ['soup', 'stock', 'clarified', 'egg']
 answers3 = ['French', '19th', 'blood', 'elegance']
 
-# Intro
 print '''Howdy Pardner! Let's play a game!'''
 
-# The level() function will prompt a user to select a difficulty level between
-# easy, medium and hard. Each choice will result in the function returning the
-# corresponding set of paragraph and answer list, as well as printing some feedback
-# on choice. If the user fails to select one of the 3 levels, even though we've
-# tried to account for capitalisation mistakes, the function will run again from the top.
+
 def level():
+	"""The level() function will prompt a user to select a difficulty level between
+	easy, medium and hard. Each choice will result in the function returning the
+	corresponding set of paragraph and answer list, as well as printing some feedback
+	on choice. If the user fails to select one of the 3 levels, even though we've
+	tried to account for capitalisation mistakes, the function will run again from the top."""
 	level_choice=raw_input('Choose a difficulty level between easy, medium and hard...  ') 
 	level_choice_feedback='''You have selected the difficulty level ''' + str(level_choice.lower()) + ''' try to solve the paragraph here below!'''
 	level_choice_invalid='''The level you've selected is not available, please try again!''' 
@@ -31,15 +31,14 @@ def level():
 		print level_choice_invalid
 		return level()
 
-
-# The quiz() function is the actual game, it only takes 1 input as both the paragraph
-# and the answer list are inputs of the level() function, which we're calling 
-# inside this one. The loop goes through the blanks[i] and checks if the corresponding 
-# answer matches by seeing if it equals to answer[i], if it doesn't we ask the 
-# user to try again. We're also counting guesses, every wrong geuss will increase 
-# this value by 1, once too many wrong guesses have been made the loop doesn't 
-# start due to the condition ''while guesses<=10'' we've set. 
 def quiz(blank):
+	"""The quiz() function is the actual game, it only takes 1 input as both the paragraph
+	and the answer list are inputs of the level() function, which we're calling 
+	inside this one. The loop goes through the blanks[i] and checks if the corresponding 
+	answer matches by seeing if it equals to answer[i], if it doesn't we ask the 
+	user to try again. We're also counting guesses, every wrong geuss will increase 
+	this value by 1, once too many wrong guesses have been made the loop doesn't 
+	start due to the condition ''while guesses<=10'' we've set. """
 	i=0
 	guesses=0
 	paragraph, answer = level()
@@ -58,8 +57,6 @@ def quiz(blank):
 	else:
 		print 'Too many wrong guesses mate...'
 
-
 quiz(blanks)     
 
-# Game Over
 print "Thanks for playing!"
